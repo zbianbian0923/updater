082d96bf8732fd22a403c024809b0d44f16d43e4
boos4721 <3.1415926535boos@gmail.com>
2020-04-29 14:29:53 +0800

    ?
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- roms/10/MIUI/make.sh
+++ roms/10/MIUI/make.sh
@@ -22,15 +22,6 @@ echo "DEVICE_PROVISIONED=1" >> $1/build.prop
 rm -rf $1/priv-app/DiracAudioControlService
 # drop FingerprintExtensionService
 rm -rf $1/app/FingerprintExtensionService
-# drop nfc
-rm -rf $1/app/NQNfcNci
 
 cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
 
-sed -i 's/<bool name="support_round_corner">true/<bool name="support_round_corner">false/' $1/etc/device_features/*
-
-sed -i "/miui.notch/d" $1/build.prop
-
-# Wifi fix
-cp -fpr $thispath/bin/* $1/bin/
-cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
